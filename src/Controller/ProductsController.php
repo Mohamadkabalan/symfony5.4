2a34d63323e26653018852d4e81809f9f638c440
Mohamad Kabalan <mkabalane@gmail.com>
2023-01-05 18:31:08 +0200

    session

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 use App\Repository\ProductRepository;
 
@@ -27,12 +28,21 @@ class ProductsController extends AbstractController
   /**
    * @Route("/products/{id}")
    */
-  public function details($id,ProductRepository $repo): Response
+  public function details($id,Request $request,ProductRepository $repo,SessionInterface $session): Response
   {
     $product=$repo->find($id);
     if($product ===null){
       throw $this->createNotFoundException("The product Does not Exist");
     }
-    return $this->render('products/details.html.twig',['product'=>$product]);
+    //add to basket handling
+    $basket=$session->get('basket',[]);
+
+    if($request->isMethod('POST')){
+      $basket[$product->getId()]=$product;
+      $session->set('basket',$basket);
+    }
+    $isInBasket=array_key_exists($product->getId(),$basket);
+
+    return $this->render('products/details.html.twig',['product'=>$product,"inBasket" => $isInBasket]);
   }
 }
