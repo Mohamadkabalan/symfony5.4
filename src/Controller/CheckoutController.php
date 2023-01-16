19a112320d4fb5307f598b656149e89c58f04c84
Mohamad Kabalan <mkabalane@gmail.com>
2023-01-16 14:56:42 +0200

    add fix

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -6,11 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+use Symfony\Component\Mailer\MailerInterfacel;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 use App\Entity\Order;
 use App\Repository\ProductRepository;
@@ -21,7 +26,7 @@ class CheckoutController extends AbstractController
   /**
    * @Route("/checkout", name="app_checkout")
    */
-  public function checkout(Request $request, ProductRepository $repo,SessionInterface $session): Response
+  public function checkout(Request $request, ProductRepository $repo,SessionInterface $session,MailerInterface $mailer): Response
   {
     if (!$this->getUser()) {
       return $this->redirectToRoute('app_login');
@@ -51,12 +56,26 @@ class CheckoutController extends AbstractController
       $entityManager= $this->getDoctrine()->getManager();
       $entityManager->persist($order);
       $entityManager->flush();
+
+      $this->sendEmailConfirmation($order,$mailer);
+
       $session->set('basket',[]);
       return $this->render('checkout/confirmation.html.twig');
     }
 
 
     return $this->render('checkout/checkout.html.twig',['total'=>$total,'form'=>$form->createView()]);
+  }
+
+  private function sendEmailConfirmation(Order $order, MailerInterface $mailer){
+    $email = (new TemplatedEmail())
+      ->from('mkabalane@gmail.com')
+      ->to(new Address($order->getEmail(), $order->getName()))
+      ->subject('Order confirmation')
+      ->htmlTemplate('emails/order.html.twig')
+      ->context(['order' => $order]);
+
+    $mailer->send($email);
   }
 
 }
